52983f8535717e990b82719e41a8e44cd75c79c0
avneesh_lenevo <singhtest20@gmail.com>
2022-08-16 00:42:44 +0100

    Added bootstrap, jquery, fontawesome and basic layout files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LeftPanelComponent } from './panels/left-panel/left-panel.component';
+import { HeaderComponent } from './panels/header/header.component';
+import { FooterComponent } from './panels/footer/footer.component';
+import { ContentComponent } from './panels/content/content.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LeftPanelComponent,
+    HeaderComponent,
+    FooterComponent,
+    ContentComponent
   ],
   imports: [
     BrowserModule,
